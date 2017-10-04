de64029a352aca31f4fad8b76db8ce00e182013a
mmazur <maksym_mazur@epam.com>
2017-10-04 17:32:41 +0200

    fetch retrieved first data!

--- src/reducers/github-widget.js
+++ src/reducers/github-widget.js
@@ -3,7 +3,13 @@ import {FETCH_GITHUB_DATA, REQUEST_GITHUB_DATA} from "../constants/actionTypes";
 export default function (state = {}, action){
     switch(action.type){
         case REQUEST_GITHUB_DATA:
-            fetch("url")
+            let url = "https://api.github.com/users/TwistedDNA/repos?per_page=1000";
+            fetch(url)
+                .then((res) => res.json())
+                .then((data) => console.log(data))
+                .catch((err) => console.log(err));
+            url = "https://api.github.com/users/TwistedDNA";
+            fetch(url)
                 .then((res) => res.json())
                 .then((data) => console.log(data))
                 .catch((err) => console.log(err));
